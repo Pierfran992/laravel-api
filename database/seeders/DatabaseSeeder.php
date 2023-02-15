84fe6590981bf6d36266949af9e914869f9e8a7b
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-02-15 13:49:26 +0100

    Creati i seeder e richiamati neel databaseseeder;

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this -> call([
+            TagSeeder::class,
+            GenreSeeder::class,
+            MovieSeeder::class,
+        ]);
     }
 }
