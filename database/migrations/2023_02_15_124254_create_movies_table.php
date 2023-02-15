096f41d6db3051b5fc8f5ac462a7a27bed7c7ece
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-02-15 14:04:57 +0100

    Create le collonne nelle table tags, genres, movies;

--- database/migrations/2023_02_15_124254_create_movies_table.php
+++ database/migrations/2023_02_15_124254_create_movies_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('movies', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 64) -> unique();
+            $table->date('release_date');
+            $table->boolean('cashOut') -> default(false);
             $table->timestamps();
         });
     }
