ee1233779a3ec0eaddc9ca726cb1c6064f418384
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-02-15 13:51:49 +0100

    Creati i model e richiamati nel MainController;

--- app/Http/Controllers/MainCotroller.php
+++ app/Http/Controllers/MainCotroller.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Tag;
+use App\Models\Genre;
+use App\Models\Movie;
+
 class MainCotroller extends Controller
 {
     // index
