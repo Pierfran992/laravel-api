6f4985498c76baaad6e8230b5ffca28ac7b67da1
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-02-16 18:14:21 +0100

    Collegato il json creato nel back-end con il front end tramite axios;

--- front_end/laravel-vue-api/src/App.vue
+++ front_end/laravel-vue-api/src/App.vue
@@ -1,5 +1,23 @@
 <script>
+import axios from 'axios';
 
+export default {
+    name: 'App',
+
+    mounted() {
+
+        axios.get('http://127.0.0.1:8000/api/v1/movie/all')
+            .then(res => {
+                const data = res.data;
+                const success = data.success;
+                const movies = data.response;
+
+                console.log(movies);
+            })
+
+            .catch(err => console.error(err));
+    }
+}
 </script>
 
 <template><h1>Hello world</h1></template>
